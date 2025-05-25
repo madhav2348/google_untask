52cc6c1bef3b90c877c861eafaa04da76ee19227
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-05-25 22:18:29 +0530

    building

--- untask_vanilla/main.js
+++ untask_vanilla/main.js
@@ -1,16 +1,25 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow , Tray , Menu} = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow();
   win.loadFile("index.html");
 }
-
-app.on("ready", () => {
-  createWindow();
-});
+let tray = null;
+app.on('ready',()=>{
+ tray = new Tray('./assets/untask.png')
+  const contextMenu = Menu.buildFromTemplate([ 
+    {label:'Open App' ,  type:'normal'}
+   ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
+  createWindow()
+})
+app.whenReady().then(() => {
+ 
+})
 
 app.on("window-all-closed", () => {
-  if (process.platform === "darwin") {
+  if (!process.platform === "darwin") {
     app.quit();
   }
 });
--- untask_vite/src/electron/main.ts
+++ untask_vite/src/electron/main.ts
@@ -1,5 +1,16 @@
-import { app, BrowserWindow } from "electron";
-app.on('ready',()=>{
-    const mainWindow = new BrowserWindow({width:800,height:600})
-    mainWindow.loadFile(app.getAppPath()+'/dist-react/index.html')
-})
+import { app, BrowserWindow, Menu, Tray } from "electron";
+
+let tray = null;
+
+app.whenReady().then(() => {
+  tray = new Tray("../assets/untask.svg");
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Open App", type: "normal" },
+  ]);
+  tray.setToolTip("Application in Tray");
+  tray.setContextMenu(contextMenu);
+});
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadFile(app.getAppPath() + "/dist-react/index.html");
+});
